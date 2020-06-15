e0c88808ef4e2c9261db2b906eacca02c3ecbfe5
eddiemccracken <edwinmccracken@gmail.com>
2020-06-15 08:03:34 -0700

    map files, slight update with json

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,19 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var myMap = L.map("map", {
+  center: [15.5994, -28.6731],
+  zoom: 3
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(myMap);
 
 var pollution = new L.LayerGroup();
+
 
 // Import CSV data
 d3.csv("fossil-fuel-co2-emissions-by-nation.csv", function(data){
@@ -13,6 +24,19 @@ d3.csv("fossil-fuel-co2-emissions-by-nation.csv", function(data){
       lat.push(data.Lat);
       lon.push(data.Lon);
       total.push(data.Total);
+      //console.log(data.Total);
+
+      var c = lat.map(function(e, i) {
+        return [e, lon[i]];
+        console.log(c)
+        //var marker = L.marker([51.5, -0.09]).addTo(mymap);
+      //   var circle = L.circle([51.508, -0.11], {
+      //     color: 'red',
+      //     fillColor: '#f03',
+      //     fillOpacity: 0.5,
+      //     radius: 500
+      // }).addTo(mymap);
+      });  
   }
 });
 
@@ -41,17 +65,6 @@ var overlayMaps = {
   Pollution: pollution
 };
 
-// Create our map, giving it the streetmap and earthquakes layers to display on load
-var myMap = L.map("map", {
-  center: [
-    37.09, -95.71
-  ],
-  zoom: 5,
-  layers: [streetmap, pollution]
-});
-
-
-
 // Create a layer control
 // Pass in our baseMaps and overlayMaps
 // Add the layer control to the map
@@ -59,33 +72,33 @@ L.control.layers(baseMaps, overlayMaps, {
   collapsed: false
 }).addTo(myMap);
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json('static/js/co2.json', function(data) {
 
   function styleInfo(feature){
     return{
       opacity: 1,
       fillOpacity: 1,
-      fillColor: getColor(feature.properties.total),
+      fillColor: getColor(feature.properties.Total),
       color:"#000000",
-      radius:getRadius(feature.properties.total),
+      radius:getRadius(feature.properties.Total),
       stroke:true,
       weight:0.5
     };
   }
 
-    function getColor(total){
+    function getColor(Total){
       switch(true){
-        case total >60000:
+        case Total >60000:
           return "purple";
-        case total >50000:
+        case Total >50000:
           return "blue";
-        case total >40000:
+        case Total >40000:
           return "red";
-        case total >30000:
+        case Total >30000:
           return "orange";
-        case total >20000:
+        case Total >20000:
           return "yellow";
-        case total <10000:
+        case Total <10000:
           return "lightgreen";
       }
     }
@@ -94,9 +107,15 @@ d3.json(queryUrl, function(data) {
       return Total;
     }
 
+    var lat = [data.Lat]
+    var lon = [data.Lon]
+    var total = [data.Total]
+    var latlng = L.latLng(lat, lon)
+
+
     L.geoJSON(data, {
-      pointToLayer: function(feature, latlng){
-        return L.circleMarker(latlng)
+      pointToLayer: function(feature, latlon){
+        return L.circleMarker(latlon)
       },
 
 // Include data on the type of fuel emissions on tool tip click
